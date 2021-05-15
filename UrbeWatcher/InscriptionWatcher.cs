a8b454dc501c59aaaa5c07e9eca1bcfa23e3f747
Diego García <dagarciam1014@gmail.com>
2021-05-15 01:54:31 -0400

    Adjusted Dependencies, Fixed Inscription Watcher
    Adjusted Project dependencies since they broke in the move
    Also I had very much misunderstood how HTTP works and now fixed my blunder

--- UrbeWatcher/InscriptionWatcher.cs
+++ UrbeWatcher/InscriptionWatcher.cs
@@ -1,5 +1,49 @@
-﻿using System;
+﻿using DiegoG.WebWatcher;
+using HtmlAgilityPack;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml.XPath;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace UrbeWatcher
 {
+    [Watcher]
+    public class InscriptionWatcher : IWebWatcher
+    {
+        private const long ChannelID = -1001188047483;
+        private const string UrbeURL = "https://inscripciones.urbe.edu/InscripcionWeb/";
+        public string Name => "InscriptionWatcher";
+
+        public TimeSpan Interval { get; } = TimeSpan.FromMinutes(10);
+
+        public async Task Check()
+        {
+            Log.Information("Checking Urbe for availability");
+            try
+            {
+                Log.Debug("Getting Web Response from " + UrbeURL);
+                HttpClient client = new();
+                var checkingResponse = await client.GetAsync(UrbeURL);
+
+                if (checkingResponse.IsSuccessStatusCode)
+                {
+                    Log.Debug("Urbe is available");
+                    OutputBot.SendTextMessage(ChannelID, $"URBE is Alive!!: {UrbeURL}\n{DateTime.Now:g}");
+                }
+                else
+                    Log.Debug("URBE is unavailable");
+            }
+            catch (WebException)
+            {
+                Log.Error($"Couldn't Connect to {UrbeURL}. Aborting Task and trying again later");
+            }
+        }
+
+        public Task FirstCheck() => Check();
+    }
 }
